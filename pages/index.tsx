f1ebab43588c7167cbb25475563b9e3eb16ff1a4
rp1222 <116277379+Robbie-Parr@users.noreply.github.com>
2023-06-11 21:39:08 +0100

    Added section snapping
    Made each section snap to the full height of the screen when you scroll to that section.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,27 +3,40 @@ import React from "react"
 import styles from '@/styles/Index.module.scss';
 
 import {Header,About,Experience,Projects,ContactForm} from "../components/LandingPageComponents"
+import Link from "next/link";
 
 const Home = () => {
   return(
-    <div className="h-full" id={styles.main}>
+    <div className="h-screen snap-y snap-mandatory overflow-scroll z-0" id={styles.main}>
 
         {/* Header section*/}
-        <Header/>
+        <div className="z-1 snap-start">
+          <Header />
+        </div>
 
         <div className="space-y-40" id={styles.text_section}>
 
+          {/* <Link href="#contact-form">Tp Contact Form</Link> */}
+
           {/* About section */}
-          <About/>
+          <section className="snap-start h-screen" id="about">{/* href="#about" */}
+            <About/>
+          </section>
 
           {/* Experience section */}
-          <Experience/>
+          <section className="snap-center h-screen" id="experience">
+            <Experience/>
+          </section>
         
           {/* Projects section */}
-          <Projects/>
+          <section className="snap-center h-screen" id="projects-section">
+            <Projects/>
+          </section>
 
           {/* Contact Form section */}
-          <ContactForm/>
+          <section className="snap-center h-screen" id="contact-form">
+            <ContactForm/>
+          </section>
         
           <footer/>
         </div>
