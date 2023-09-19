220fa3c9ef69f3017f5761ac6ea70ee813c4fe7c
rp1222 <116277379+Robbie-Parr@users.noreply.github.com>
2023-09-19 10:46:13 +0100

    Added Skills title tag

--- components/LandingPageComponents/Skills/SkillsCarousel/index.tsx
+++ components/LandingPageComponents/Skills/SkillsCarousel/index.tsx
@@ -12,10 +12,15 @@ const SkillsCarousel = () => {
     const skills = useSkills();
 
     return(
+        <div style={{flexDirection:"row",display:"flex"}}>
+        <div id={styles.title}>
+            <h1>Skills</h1>
+        </div>
         <div id={styles.carousel_container}>
             {skills.reverse().map((skill) => 
                 <Card key={skill.name} {...skill}/>
                 )}
+        </div>
         </div>
     )
 }
